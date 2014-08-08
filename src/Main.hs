e9d9ff4b9ded01c8112a0d203ed04eebaad68e07
mattlock <mswhitlock@crimson.ua.edu>
2014-08-08 16:01:36 -0500

    Adding basic framework for SudokuBoard, subject to change to Funtion type, Monad, etc

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,25 @@
 module Main where
 
-type SudokuBoard = (Int -> Int) -> Int
+import Data.Array.IO
+
+type SudokuBoard = [[Maybe Integer]]
+--type SudokuBoard = (Int, Int) -> Maybe Integer
+
 
 main::IO()
 main = do
-    inputPuzzle <- readFile "../puzzle.txt"
-    mapM_ putStrLn (lines inputPuzzle)
+    contents <- getContents
+    print $ solve $ initBoard nothingBoard contents
+    
+-- | 'nothing board' function creates a 9x9 board filled with Nothing
+nothingBoard :: SudokuBoard
+nothingBoard = map (\x -> map (\y -> (Nothing)) [1..9]) [1..9]
+
+-- | 'initBoard' function takes empty board and file contents and returns the initialized board (TO BE IMPLEMENTED) 
+initBoard :: SudokuBoard -> String -> SudokuBoard
+initBoard board str = board
+
+-- | 'solve' function takes initialized board and returns the solved board (TO BE IMPLEMENTED) 
+solve :: SudokuBoard -> SudokuBoard
+solve board = board
+    
