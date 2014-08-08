1098a079d475bec3361ac894a4ebb16808fe161d
mattlock <mswhitlock@crimson.ua.edu>
2014-08-08 16:32:35 -0500

    Added a rough sketch of what it would look like to have this with the SudokuBoard defined as a function

--- src/Main.hs
+++ src/Main.hs
@@ -3,17 +3,18 @@ module Main where
 import Data.Array.IO
 
 type SudokuBoard = [[Maybe Integer]]
---type SudokuBoard = (Int, Int) -> Maybe Integer
+-- type SudokuBoard = (Integer, Integer) -> Maybe Integer COMMENTED OUT ALTERNATE TYPE
 
 
 main::IO()
 main = do
     contents <- getContents
-    print $ solve $ initBoard nothingBoard contents
+    print $ display $ solve $ initBoard nothingBoard contents
     
 -- | 'nothing board' function creates a 9x9 board filled with Nothing
 nothingBoard :: SudokuBoard
 nothingBoard = map (\x -> map (\y -> (Nothing)) [1..9]) [1..9]
+-- nothingBoard (x, y) = Nothing //COMMENTED OUT ROUGHLY WHAT THIS WOULD LOOK LIKE (by that, I mean it compiled okay)
 
 -- | 'initBoard' function takes empty board and file contents and returns the initialized board (TO BE IMPLEMENTED) 
 initBoard :: SudokuBoard -> String -> SudokuBoard
@@ -23,3 +24,14 @@ initBoard board str = board
 solve :: SudokuBoard -> SudokuBoard
 solve board = board
     
+-- | 'display' function to convert a SudokuBoard to String representation
+display :: SudokuBoard -> String
+display board = ""
+{-| display board = displaySpace (board "" 1 1)
+
+-- | 'displaySpace' function to convert a SudokuBoard to String representation
+displaySpace :: (SudokuBoard -> String -> Int -> Int) -> String
+displaySpace board str x 10 = str
+displaySpace board str 10 y = displaySpace (board str + "\n" 1 (y + 1))
+displaySpace board str x y = displaySpace (board str + (board x y) (x + 1) y)
+-}
