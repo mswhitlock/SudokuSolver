af01e38ca0f64dce86d131039977e0f15213b216
Connor Lawson <rconnorlawson@gmail.com>
2014-08-09 17:51:08 -0500

    First implementation of function display.

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,6 @@ import Data.Char
 import Data.Maybe
 
 type SudokuBoard = Array (Int, Int) (Maybe Int)
--- type SudokuBoard = (Integer, Integer) -> Maybe Integer COMMENTED OUT ALTERNATE TYPE
 
 main::IO()
 main = interact (display . solve . initBoard)
@@ -22,24 +21,22 @@ initBoard str = array ((1,1), (9,9)) [((x, y), value) | x <- [1..9]
                                                       , let value = toMaybeInt $ lines str !! (y - 1) !! (x - 1)]
 
 toMaybeInt :: Char -> Maybe Int
-toMaybeInt c | isDigit c = Just (read [c])
-             | c == ' '  = Nothing
-             | otherwise = error "Invalid input board"
+toMaybeInt c | c == ' '  = Nothing
+	     | isDigit c = Just (digitToInt c)
+	     | otherwise = error "Invalid input board"
+
+-- | 'display' function to convert a SudokuBoard to String representation
+display :: SudokuBoard -> String
+display board = unlines [rowToString board y | y <- [1..9]]
+
+rowToString :: SudokuBoard -> Int -> String
+rowToString board y = [fromMaybeInt $ board ! (x,y) | x <- [1..9]]
+
+fromMaybeInt :: Maybe Int -> Char
+fromMaybeInt Nothing  = ' '
+fromMaybeInt (Just x) = intToDigit x
 
 -- | 'solve' function takes initialized board and returns the solved board (TO BE IMPLEMENTED) 
 solve :: SudokuBoard -> SudokuBoard
 solve board = board
     
--- | 'display' function to convert a SudokuBoard to String representation
-display :: SudokuBoard -> String
-display board = ""
--- COMMENTED OUT DISPLAY OF BOARD BASED ON FUNCTION
--- THIS DOESN'T COMPILE AND IT'S ACTUALLY PRETTY UGLY, BUT I THINK THE ISSUE IS JUST HORRIBLE SYNTAX
-{-| display board = displaySpace (board "" 1 1)
-
--- | 'displaySpace' function to convert a SudokuBoard to String representation
-displaySpace :: (SudokuBoard -> String -> Int -> Int) -> String
-displaySpace board str x 10 = str
-displaySpace board str 10 y = displaySpace (board str + "\n" 1 (y + 1))
-displaySpace board str x y = displaySpace (board str + (board x y) (x + 1) y)
--}
